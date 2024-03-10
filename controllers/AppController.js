cfae8e7cc742bd26b78692d4493bd2551efce733
elyse502 <elyseniyibizi502@gmail.com>
2024-03-10 09:16:09 -0700

    Add Solution to Task 2. First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,17 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class AppController {
-  static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
   }
 
-  static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+  static async getStats(req, res) {
+    const files = await dbClient.nbFiles();
+    const users = await dbClient.nbUsers();
+    res.status(200).json({ users, files });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,26 @@
-import { Router } from 'express';
+/* eslint-disable linebreak-style */
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = Router();
+const router = express.Router();
 
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
-
-router.get('/connect', AuthController.getConnect);
-
-router.get('/disconnect', AuthController.getDisconnect);
-
 router.get('/users/me', UsersController.getMe);
 
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
 router.post('/files', FilesController.postUpload);
-
 router.get('/files/:id', FilesController.getShow);
-
 router.get('/files', FilesController.getIndex);
-
 router.put('/files/:id/publish', FilesController.putPublish);
-
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
 router.get('/files/:id/data', FilesController.getFile);
 
-module.exports = router;
+export default router;
