d36f86c8d624645db3dd78618c23c6cec7a3ab08
elyse502 <elyseniyibizi502@gmail.com>
2024-03-10 08:59:28 -0700

    Add Solution to Task 2. First API

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable linebreak-style */
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
+
