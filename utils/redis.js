e43eeac55870440923c1faf920c3e5cee57c53b8
elyse502 <elyseniyibizi502@gmail.com>
2024-03-10 08:14:24 -0700

    Add Solution to Task 0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// Class to define methods for commonly used redis commands.
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // Check connection status and report
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // Get value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // Set key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // del key value pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
